73209e2c9a90e70ee5060d2978ed3edc74cf830e
wovalle <willyovalle16@gmail.com>
2016-03-03 20:03:43 -0400

    Enhancement: adding test for createContact

--- tests/test.js
+++ tests/test.js
@@ -4,10 +4,10 @@ var GoogleContacts = require('../').GoogleContacts;
 var assert = require('assert');
 var contactsTested = false;
 var contactTested = false;
+var contactCreated = false;
 
 var c = new GoogleContacts({
-  token: process.env.GOOGLE_TOKEN,
-  id: process.env.GOOGLE_CONTACT_ID
+  token: process.env.GOOGLE_TOKEN
 });
 
 c.getContacts(function (err, contacts) {
@@ -22,10 +22,34 @@ c.getContact(function (err, contact) {
   assert.ok(typeof contact === 'object', 'Contact is not an object');
   console.log(contact);
   contactTested = true;
-});
+}, {id: process.env.GOOGLE_CONTACT_ID});
+
+var contactToCreate = {
+  entry: {
+    name: {
+      fullName: 'foo bar'
+    },
+    email: [{
+      primary: true,
+      address: 'foobar@foo.bar',
+      type: 'home'
+    }],
+    phoneNumber: [{
+      type: 'work',
+      phoneNumber: '555555555'
+    }]
+  }
+};
+
+c.createContact(function (err, contact) {
+  if (err) throw err;
+  assert.ok(typeof contact === 'object', 'Contact is not an object');
+  contactCreated = true;
+}, contactToCreate);
+
 
 process.on('exit', function () {
-  if (!contactsTested || !contactTested) {
+  if (!contactsTested || !contactTested || !contactCreated) {
     throw new Error('contact test failed');
   }
 });
